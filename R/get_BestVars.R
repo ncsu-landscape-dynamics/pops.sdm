b246ce77eea8396c57a931a4c3879a2bc8dbd45a
spacetime-ecology <ben.seliger@gmail.com>
2023-02-01 15:53:39 -0500

    removed PA.table argument, now defunct

--- R/get_BestVars.R
+++ R/get_BestVars.R
@@ -46,8 +46,7 @@ get_BestVars <- function(envi, pts){
                                                          resp.xy = myXY2,
                                                          resp.name = myName2,
                                                          PA.nb.rep = 1,
-                                                         PA.strategy = 'user.defined',
-                                                         PA.table = PA.df)
+                                                         PA.strategy = 'user.defined') # PA.table = PA.df
 
                           # 3. Computing the models
                           myModels <- biomod2::BIOMOD_Modeling(data = myData,
