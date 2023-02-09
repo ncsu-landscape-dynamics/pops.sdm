53a92553efaac00608e04d88903699c6cee61006
spacetime-ecology <ben.seliger@gmail.com>
2023-02-09 11:34:30 -0500

    added countries function

--- R/l48.R
+++ R/l48.R
@@ -3,7 +3,8 @@
 
 l48 <- function(){
   bbox <- c(24.5, -125, 49.5, -66.5)
-  us_can <- borders[borders$SOVEREIGNT%in%c('United States of America', 'Canada'),]
+  countries <- terra::vect('Q:\\Shared drives\\Data\\Original\\ne_10m_admin_0_countries_lakes\\ne_10m_admin_0_countries_lakes.shp')
+  us_can <- countries[countries$SOVEREIGNT%in%c('United States of America', 'Canada'),]
   us_can <- terra::crop(us_can, ext(c(bbox[2], bbox[4], bbox[1], bbox[3])))
   return(us_can)
 }
