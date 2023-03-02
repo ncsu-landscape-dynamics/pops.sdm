37cf9c144c8bd954fb83705b07e0238f568f27b1
spacetime-ecology <ben.seliger@gmail.com>
2023-03-02 09:08:33 -0500

    more algos

--- R/get_BestVars.R
+++ R/get_BestVars.R
@@ -23,7 +23,7 @@ get_BestVars <- function(envi, pts){
    names(algos) <- c('SRE', 'GLM', 'RF', 'MAXENT.Phillips')
   # algos <- list(c('SRE'), c('RF'), c('MAXENT'))
   # names(algos) <- c('SRE', 'RF', 'MAXENT')
-  algos <- list(c('SRE')); names(algos) <- c('SRE')
+  #algos <- list(c('SRE')); names(algos) <- c('SRE')
   evals <- c('CSI', 'ROC', 'TSS', 'ACCURACY', 'ETS', 'BIAS', 'KAPPA')
 
   i <- 1#; t.list <- list()
