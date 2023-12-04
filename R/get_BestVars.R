8b8d03e8b90673ae1064aaab9199cb8436fb4981
spacetime-ecology <ben.seliger@gmail.com>
2023-12-04 15:59:29 -0500

    eval to full model

--- R/get_BestVars.R
+++ R/get_BestVars.R
@@ -14,10 +14,10 @@ get_BestVars <- function(envi, pts, clust){
 
   # # Format Data with pseudo-absences : user.defined method
   myPA <- ifelse(myResp == 1, TRUE, FALSE)
-  myPAtable <- data.frame(PA1 = myPA, PA2 = myPA, PA3 = myPA, PA4 = myPA, PA5 = myPA)
+  myPAtable <- data.frame(PA1 = myPA)#, PA2 = myPA, PA3 = myPA, PA4 = myPA, PA5 = myPA)
   pa.x <- vector()
   for (i in 1:ncol(myPAtable)){
-    pa.s <- sample(which(myPAtable[, i] == FALSE), length(pts.t)*3)
+    pa.s <- sample(which(myPAtable[, i] == FALSE), length(pts.t)) #length(pts.t)*3)
     myPAtable[pa.s, i] <- TRUE
     pa.x <- c(pa.x, pa.s)
   }
@@ -67,18 +67,20 @@ get_BestVars <- function(envi, pts, clust){
                               myModels <- biomod2::BIOMOD_Modeling(bm.format = myData, #bm.format = myData,
                                                                    bm.options = myOptions, #bm.options = myOptions,
                                                                    models = i.algo,
-                                                                   CV.strategy='kfold',
-                                                                   CV.k=5,
-                                                                   metric.eval = evals
+                                                                   CV.perc=.999,
+                                                                   CV.nb.rep=1,
+                                                                   # CV.strategy='kfold',
+                                                                   # CV.k=5,
+                                                                   metric.eval = evals,
                                                                    #save.output = T, # recommended to leave true
                                                                    #scale.models = F, #experimental don't use
-                                                                   #CV.do.full.models = T
+                                                                   CV.do.full.models = T
                               )
 
                               # Evaluation
                               #options(digits = 7)
                               myEval <- biomod2::get_evaluations(myModels)
-                              eval.v <- myEval$validation
+                              eval.v <- myEval$calibration[grepl('_allData_allRun', myEval$full.name)] #eval.v <- myEval$validation
                               return(mean(eval.v))
                             })
       row.names(k.test) <- k.names
