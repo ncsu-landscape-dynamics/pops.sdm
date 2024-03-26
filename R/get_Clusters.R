6e4cc2b34c139152bdece8fa08a763a5c17ef5da
spacetime-ecology <ben.seliger@gmail.com>
2024-03-26 14:50:24 -0400

    created cluster function

---
+++ R/get_Clusters.R
@@ -0,0 +1,14 @@
+# require(klaR)
+# require(terra)
+#' @export
+
+get_Clusters <- function(envi, n){
+  #ev.samp <- sample.int(n=terra::ncell(envi[[1]]), size=100000)
+  #envi.df <- terra::extract(envi, ev.samp)
+  envi.df <- terra::values(envi, na.rm=T, dataframe=T)
+  envi.cc <- klaR::corclust(x=envi.df)
+  #envi.pl <- plot(envi.cc, mincor=.7, selection='numeric')
+  #envi.tr <- klaR::cvtree(envi.cc, k=6)
+  envi.tr <- klaR::cvtree(envi.cc, mincor=.1)
+  return(envi.tr)
+}
