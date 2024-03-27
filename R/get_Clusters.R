9ea3298e6d0ca346cb1274dbf552dda5bb2f8880
spacetime-ecology <ben.seliger@gmail.com>
2024-03-27 11:52:57 -0400

    null argument

--- R/get_Clusters.R
+++ R/get_Clusters.R
@@ -2,13 +2,13 @@
 # require(terra)
 #' @export
 
-get_Clusters <- function(envi, n){
+get_Clusters <- function(envi, n=NULL){
   #ev.samp <- sample.int(n=terra::ncell(envi[[1]]), size=100000)
   #envi.df <- terra::extract(envi, ev.samp)
   envi.df <- terra::values(envi, na.rm=T, dataframe=T)
   envi.cc <- klaR::corclust(x=envi.df)
   #envi.pl <- plot(envi.cc, mincor=.7, selection='numeric')
-  if(exists('n')){envi.tr <- klaR::cvtree(envi.cc, k=n)}
-  if(!exists('n')){envi.tr <- klaR::cvtree(envi.cc, mincor=.1)}
+  if(!is.null(n)){envi.tr <- klaR::cvtree(envi.cc, k=n)}
+  if(is.null(n)){envi.tr <- klaR::cvtree(envi.cc, mincor=.1)}
   return(envi.tr)
 }
