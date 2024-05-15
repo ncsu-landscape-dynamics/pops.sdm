3dddb134ad3473ead3c4666f655e2e0725fce80c
spacetime-ecology <ben.seliger@gmail.com>
2024-05-15 15:39:03 -0400

    sampling

--- R/get_Clusters.R
+++ R/get_Clusters.R
@@ -2,12 +2,22 @@
 # require(terra)
 #' @export
 
-get_Clusters <- function(envi, n=NULL){
-  #ev.samp <- sample.int(n=terra::ncell(envi[[1]]), size=100000)
-  #envi.df <- terra::extract(envi, ev.samp)
-  envi.df <- terra::values(envi, na.rm=T, dataframe=T)
-  envi.cc <- klaR::corclust(x=envi.df)
-  #envi.pl <- plot(envi.cc, mincor=.7, selection='numeric')
+get_Clusters <- function(envi, n=NULL, samp=NULL){
+  if(!is.null(samp)){
+    # ev.nonna <- !is.na(envi[[1]])
+    # ev.cells <- which(terra::values(ev.nonna)==T)
+    # ev.samp <- sample(x=ev.cells, size=100000000)
+    if(samp>90000000){samp <- 90000000}
+    envi.x <- exactextractr::exact_extract(envi, as(domain, 'Spatial'), force_df=T)
+    envi.b <- dplyr::bind_rows(envi.x); envi.b <- envi.b[,1:(length(envi.b)-1)]
+    remove(envi.x)
+    envi.df <- na.omit(envi.b)
+    envi.df2 <- envi.df[sample(1:nrow(envi.df), size=samp),]
+  }
+  if(is.null(samp)){
+    envi.df2 <- terra::values(envi, na.rm=T, dataframe=T)
+  }
+  envi.cc <- klaR::corclust(x=envi.df2, method='complete') #envi.pl <- plot(envi.cc, mincor=.7, selection='numeric')
   if(!is.null(n)){envi.tr <- klaR::cvtree(envi.cc, k=n)}
   if(is.null(n)){envi.tr <- klaR::cvtree(envi.cc, mincor=.1)}
   return(envi.tr)
